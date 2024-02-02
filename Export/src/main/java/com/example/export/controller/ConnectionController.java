b26e23c819da1e0d2bef24aba6a52baa3fa3ed2d
Akshata Kodak <akshatakodak@gmail.com>
2024-02-02 17:49:45 +0530

    import

--- Export/src/main/java/com/example/export/controller/ConnectionController.java
+++ Export/src/main/java/com/example/export/controller/ConnectionController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.example.demo.model.Connection;
+import com.example.export.model.Connection;
 
 @RestController
 @RequestMapping("/connection")
